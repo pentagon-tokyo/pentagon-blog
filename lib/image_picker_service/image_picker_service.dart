ad14bd5b150d6df153195f99573502911adfdae1
RYO1223 <kuwanor0811@gmail.com>
2023-06-16 15:31:16 +0900

    feat: comment

--- lib/image_picker_service/image_picker_service.dart
+++ lib/image_picker_service/image_picker_service.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
+/// このクラスは実際には呼び出されないが、IDEの補完を効かせるために定義している
 class ImagePickerService {
+  /// このメソッドは実際には呼び出されない
   Future<XFile?> pickImage(BuildContext context) async {
     throw UnsupportedError('');
   }
