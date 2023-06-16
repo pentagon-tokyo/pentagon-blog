9b5d6c713533a7a73ab67b31d94565b7b33b6a93
RYO1223 <kuwanor0811@gmail.com>
2023-06-16 15:31:22 +0900

    fix: typo

--- lib/image_picker_service/export.dart
+++ lib/image_picker_service/export.dart
@@ -1,3 +1,3 @@
 export 'package:exif/image_picker_service/image_picker_service.dart'
-    if (dart.library.html) 'package:exif/image_picker_service/imaeg_picker_service_for_web.dart'
+    if (dart.library.html) 'package:exif/image_picker_service/image_picker_service_for_web.dart'
     if (dart.library.io) 'package:exif/image_picker_service/image_picker_service_for_mobile.dart';
